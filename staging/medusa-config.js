55edb548f50d63ef41c374ab612ffaf517188f5a
mehnz <32431973+Hermitauge@users.noreply.github.com>
2024-06-29 18:20:50 -0700

    staging config

--- staging/medusa-config.js
+++ staging/medusa-config.js
@@ -15,68 +15,60 @@ switch (process.env.NODE_ENV) {
   default:
     ENV_FILE_NAME = ".env";
     break;
-}
-
-try {
+  } try {
   dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
-} catch (e) { }
+  } catch (e) { }
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+const ADMIN_CORS = process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
-
-const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
-
+const DATABASE_TYPE = process.env.DATABASE_TYPE || "sqlite";
+const DATABASE_URL = process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
-const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
-const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
-const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
-const cloudinaryConfigured = CLOUDINARY_CLOUD_NAME && CLOUDINARY_API_KEY && CLOUDINARY_API_SECRET;
+const BACKEND_URL = process.env.BACKEND_URL || "localhost:9000";
+const STORE_URL = process.env.STORE_URL || "localhost:8000";
+
+// RESEND
+const RESEND_API_KEY = process.env.RESEND_API_ID;
+const RESEND_ENABLE_ENDPOINT = process.env.RESEND_ENABLE_ENDPOINT;
+const RESEND_TEMPLATE_PATH = process.env.RESEND_TEMPLATE_PATH;
+const RESEND_SUBJECT_TEMPLATE_TYPE = process.env.RESEND_SUBJECT_TEMPLATE_TYPE;
+const RESEND_BODY_TEMPLATE_TYPE = process.env.RESEND_BODY_TEMPLATE_TYPE;
+const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
+const ADMIN_URL = process.env.ADMIN_URL || "localhost:7000";
+const RESEND_USER_PASSWORD_RESET = process.env.RESEND_USER_PASSWORD_RESET;
+const RESEND_USER_INVITE = process.env.RESEND_USER_INVITE;
 
 const ADMIN_APP_PORT = process.env.PORT || 7001;
 
-const fileServicePlugin = cloudinaryConfigured
-  ? {
-    resolve: `medusa-file-cloudinary`,
-    options: {
-      cloud_name: CLOUDINARY_CLOUD_NAME,
-      api_key: CLOUDINARY_API_KEY,
-      api_secret: CLOUDINARY_API_SECRET,
-      secure: true,
-    },
-  }
-  : {
-    resolve: `@medusajs/file-local`,
-    options: {
-      upload_dir: "uploads",
-    },
-  };
-
-const plugins = [
-  `medusa-fulfillment-manual`,
-  `medusa-payment-manual`,
-  fileServicePlugin,
-  {
-    resolve: "@medusajs/admin",
-    /** @type {import('@medusajs/admin').PluginOptions} */
-    options: {
-      autoRebuild: true,
-      path: "/app",
-      develop: {
-        open: process.env.OPEN_BROWSER !== "false",
-        port: ADMIN_APP_PORT,
+  const plugins = [
+    `medusa-fulfillment-manual`,
+    `medusa-payment-manual`,
+    {
+      resolve: `@medusajs/file-local`,
+      options: {
+        upload_dir: "uploads",
       },
     },
-  },
+    {
+      resolve: "@medusajs/admin",
+      /** @type {import('@medusajs/admin').PluginOptions} */
+      options: {
+        autoRebuild: true,
+        path: "/",
+        develop: {
+          open: process.env.OPEN_BROWSER !== "false",
+          port: ADMIN_APP_PORT,
+        },
+      },
+    },
 ];
 
 const modules = {
-  /*eventBus: {
+  eventBus: {
     resolve: "@medusajs/event-bus-redis",
     options: {
       redisUrl: REDIS_URL
@@ -85,19 +77,20 @@ const modules = {
   cacheService: {
     resolve: "@medusajs/cache-redis",
     options: {
-      redisUrl: REDIS_URL
+      redisUrl: REDIS_URL,
+      ttl: 30
     }
-  },*/
+  },
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
 const projectConfig = {
   jwtSecret: process.env.JWT_SECRET,
   cookieSecret: process.env.COOKIE_SECRET,
-  store_cors: STORE_CORS,
   database_url: DATABASE_URL,
+  database_type: DATABASE_TYPE,
+  store_cors: STORE_CORS,
   admin_cors: ADMIN_CORS,
-  // Uncomment the following lines to enable REDIS
   redis_url: REDIS_URL
 };
 
